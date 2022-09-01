d844b640fed67f819f889935534f76ae6bc6da67
SabikaFarrukh <110960972+SabikaFarrukh@users.noreply.github.com>
2022-09-01 13:25:33 +0500

    Add files via upload

---
+++ users/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+class userdoesnotexists(models.Model):
+    userdoesnotexists=models.CharField(("userdoesnotexists"), max_length=50)
+    def self():
+        return(self)
